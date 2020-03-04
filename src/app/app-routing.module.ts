e371a695d03ea44aade11d1244675ac673def9a9
pl-jay <lakshan.jap@gmail.com>
2020-03-04 23:36:23 +0530

    front-end-app/basic component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './_components/login/login.component';
+import { HomeComponent } from './_components/home/home.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'login', component: LoginComponent },
+  { path:'home', component: HomeComponent },
+  { path:'', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './_components/login/login.component';
+import { HomeComponent } from './_components/home/home.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+
+// import material modules
+import { MaterialModule } from './material.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
